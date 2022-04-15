68505c3dcc6374c1fbc6c47f2c2bdf1b82af3435
Gavin M <gavinmumpower96@gmail.com>
2022-04-14 20:56:02 -0400

    pushing features for 5-day, all weather chars, README

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple Weather DashBoard
+      ## Description
+      This is a a minimalist Weather Dashboard application display use the Open Weather APi. This app allows a user to search for cities, and obtain information such as Temperature, humidity, Wind Speed, and a UV index as well. Use of display to show a 5-day weather forecast.
+      ## Table of Contents:
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Contributing](#contributing)
+      - [Tests](#tests)
+      - [Questions](#questions)
+      ## Installation
+      
+      ## Usage
+      Search for your interested city or state, and you will find related weather information to that search in a 5-day forecast.
+      ## License
+      ### ![MIT](https://img.shields.io/badge/license-MIT-green)
+      ### [https://choosealicense.com/licenses/mit/](https://choosealicense.com/licenses/mit/)
+      
+      ## Contributing
+      
+      ## Tests
+      
+      
+      ## Questions
+      [Github Profile](https://github.com/) | Send me an Email 
+    
--- assets/script.js
+++ assets/script.js
@@ -116,3 +116,214 @@ class DailyWeather {
       hideElement('daily-weather-row');
     }
   }
+  
+  //Search button event listener
+  document.getElementById('search-city').addEventListener('submit', submitSearch);
+  //Search history event listener
+  document.getElementById('search-history').addEventListener('click', submitSearchHistoryEntry);
+  
+  /***** Process search history item click *****/
+  function submitSearchHistoryEntry(event) {
+    event.preventDefault();
+  
+    if (event.target.matches('.search-entry')) {
+      startGettingWeatherData(event.target.dataset.cityName);
+    }
+  }
+  
+  /***** Handling user input *****/
+  //when search submitted
+  function submitSearch(event) {
+    event.preventDefault();
+    resetSearchError();
+  
+    const userInput = getUserInput();
+  
+    if (userInput) {
+      startGettingWeatherData(userInput);
+    } else {
+      document.getElementById('city-name').value = '';
+      showSearchError('Please enter a city name');
+    }
+  }
+  
+  function getUserInput() {
+    let userInput = document.getElementById('city-name').value;
+    userInput = userInput.replace(/\s+/g,' ');
+    userInput = userInput.trim();
+    return userInput;
+  }
+  
+  //errors
+  function resetSearchError() {
+    document.getElementById('city-name').classList.remove('border-danger');
+    document.getElementById('error').textContent = '';
+  }
+  
+  function showSearchError(message) {
+    document.getElementById('city-name').classList.add('border-danger');
+    document.getElementById('error').textContent = message;
+  }
+  
+  /***** Getting weather data *****/
+  const API_KEY = '9ddb9de285b4900eb2512aca9a5b8264';
+  
+  function startGettingWeatherData(cityName) {
+    // getFiveDayForecast(cityName, API_KEY);
+    if (!isCurrentlyDisplayed(cityName)) {
+      fetchData(getCurrentWeatherURL(cityName), processCurrentWeatherData);
+    }
+  }
+  
+  //general function to fetch data
+  function fetchData(queryURL, nextAction) {
+    fetch(queryURL)
+      .then(function(response){
+        return response.json();
+      }).then(nextAction);
+  }
+  
+  //processing data functions
+  function processCurrentWeatherData(data) {
+    if (data.cod != 200) {
+      showSearchError('City not found');
+    } else {
+      const weatherData = new WeatherData(
+        data.name,
+        data.coord.lat,
+        data.coord.lon);
+      const currentDay = new CurrentWeather(
+        data.main.temp,
+        data.main.humidity,
+        data.wind.speed,
+        data.weather[0].description)
+        .setDate(data.dt)
+        .setIconName(data.weather[0].icon, data.dt);
+      weatherData.setCurrentDay(currentDay);
+      //make next call to one call api
+      fetchData(getOneCallURL(data.coord.lat, data.coord.lon), function(data) {
+        processOneCallData(data, weatherData);
+      });
+    }
+  }
+  
+  function processOneCallData(data, weatherData) {
+    const weatherObj = weatherData;
+    weatherObj.setOneCallInfo(data.daily);
+  
+    displayInformation(weatherObj);
+    saveLastSearchedCity(weatherObj.city.name);
+  }
+  
+  //getting URL queries
+  function getCurrentWeatherURL(cityName) {
+    return 'https://api.openweathermap.org/data/2.5/weather?'
+     + `q=${cityName}`
+     + '&units=imperial'
+     + `&appid=${API_KEY}`;
+  }
+  
+  function getOneCallURL(lat, lon) {
+    return 'https://api.openweathermap.org/data/2.5/onecall?'
+    + `lat=${lat}`
+    + `&lon=${lon}`
+    + '&units=imperial'
+    + `&appid=${API_KEY}`;
+  }
+  
+  /***** Display functions *****/
+  function displayInformation(weatherObj) {
+    hideElement('start-text');
+    setCityInfo(weatherObj.city.name);
+    displayOverviewCard(weatherObj.currentDay, weatherObj.city.name);
+    displayFiveDayForecast(weatherObj.nextFiveDays);
+    displayNewSearchEntry(weatherObj.city.name);
+  }
+  
+  function setCityInfo(cityName) {
+    const cityInfo = document.getElementById('city-info');
+    cityInfo.setAttribute('data-city', cityName);
+  }
+  
+  function displayOverviewCard(currentDay, cityName) {
+    const currentWeather = document.getElementById('current-weather');
+  
+    currentWeather.innerHTML =
+      `<div class="card-body">
+        <h2 class="d-inline-block mr-3">${cityName} ${currentDay.date}</h2>
+        <img class="d-inline-block" src="https://openweathermap.org/img/wn/${currentDay.icon.name}@2x.png" alt="${currentDay.icon.description}">
+        <p>Temperature: ${currentDay.temp} &#176;F</p>
+        <p>Humidity: ${currentDay.humidity}&#37;</p>
+        <p>Wind Speed: ${currentDay.windSpeed} MPH</p>
+        <p>UV Index: <span id="current-uv-index" class="${currentDay.uv.color} py-1 px-2 rounded">${currentDay.uv.index}</span></p>
+      </div>`;
+    showElement('curr-weather-row');
+  }
+  
+  function displayFiveDayForecast(dayList) {
+    const fiveDayForecastCards = document.getElementById('five-day-forecast-cards');
+    fiveDayForecastCards.innerHTML = '';
+    for (day of dayList) {
+      fiveDayForecastCards.innerHTML +=
+        `<div class="col-lg">
+          <div class="card bg-primary text-white">
+            <div class="card-body d-flex flex-column justify-content-center align-items-center">
+              <p class="h5">${day.date}</p>
+              <img class="mb-3" src="https://openweathermap.org/img/wn/${day.icon.name}@2x.png" alt="${day.icon.description}">
+              <p>Temp: ${day.temp} &#176;F</p>
+              <p>Humidity: ${day.humidity}&#37;</p>
+            </div>
+          </div>
+        </div>`;
+    }
+    showElement('daily-weather-row');
+  }
+  
+  function displayNewSearchEntry(cityName) {
+    const searchHistoryList = document.getElementById('search-history');
+  
+    if (!isExistingSearch(cityName, searchHistoryList)) {
+      //add if duplicate not found
+    searchHistoryList.innerHTML +=
+    `<li class="list-group-item p-0">
+      <button class="search-entry w-100 h-100 btn btn-link" data-city-name="${cityName}">${cityName}</button>
+    </li>`;
+    }
+  }
+  
+  /***** Show/Hide functions *****/
+  function hideElement(name) {
+    document.getElementById(name).setAttribute('style', 'display: none!important');
+  }
+  
+  function showElement(name) {
+    document.getElementById(name).style.display = 'block';
+  }
+  
+  /***** Condition Checkers *****/
+  function isExistingSearch(cityName, searchHistoryList) {
+    const searchHistoryItems = searchHistoryList.children;
+    //keep from adding duplicates
+    for (item of searchHistoryItems) {
+      console.log(item.firstElementChild.dataset.cityName);
+      if (item.firstElementChild.dataset.cityName === cityName) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function isCurrentlyDisplayed(cityName) {
+    const currentDisplay = document.getElementById('city-info').dataset.city;
+    return currentDisplay === cityName ? true : false;
+  }
+  
+  /***** Local storage *****/
+  function saveLastSearchedCity(cityName) {
+    localStorage.setItem('lastSearchedCity', cityName);
+  }
+  
+  function getLastSearchedCity() {
+    return localStorage.getItem('lastSearchedCity');
+  }
+  
